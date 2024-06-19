959aaa5e7a8fadced441e3be699a586391985b66
JessePel <jesse1.peltola@outlook.com>
2024-06-19 14:56:23 +0300

    Part 2 Done

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,41 +1,45 @@
-const Header = ({title}) => <h1>{title}</h1>
+const Header = ({ title }) => <h1>{title}</h1>
 
-// const Total = ({parts}) => <p><b>total of {parts.exercises.reduce((a, b) => a + b, 0)} exercises </b></p>
+const Total = ({ parts }) => {
+    const total = parts.reduce((acc, b) => { 
+        return acc + b.exercises}, 0)
 
-const Total = ({parts}) => {
-    let total = 0
-    for (let i = 0; i < parts.length; i++) {
-        total += parts[i].exercises
-    }
-    return (
-        <div>
-            <p><b>total of {total} exercises</b></p>
-        </div>
-    )
+    return (<p><b>total of {total} exercises </b></p>)
 }
-const Content = ({course}) => {
+// const Total = ({parts}) => {
+//     let total = 0
+//     for (let i = 0; i < parts.length; i++) {
+//         total += parts[i].exercises
+//     }
+//     return (
+//         <div>
+//             <p><b>total of {total} exercises</b></p>
+//         </div>
+//     )
+// }
+const Content = ({ course }) => {
     return (
         <div>
-            {course.map(part => 
+            {course.map(part =>
                 <Part key={part.id} part={part} />
             )}
         </div>
     )
 }
 
-const Part = ({part}) => {
+const Part = ({ part }) => {
     return (
         <p>{part.name} {part.exercises}</p>
     )
 }
 
-const Course = ({course}) => {
+const Course = ({ course }) => {
     return (
         <div>
-        <Header title={course.name} />
-        <Content course={course.parts} />
-        <Total parts = {course.parts} />
-        {/* <Total parts = {course.parts} /> */}
+            <Header title={course.name} />
+            <Content course={course.parts} />
+            <Total parts={course.parts} />
+            
         </div>
     )
 }
